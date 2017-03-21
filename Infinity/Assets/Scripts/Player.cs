dfb72a481bf763caceb223f77b26223fe91ca974
aschil <ajschil6@gmail.com>
2017-03-20 21:39:29 -0500

    Added Player Movement

---
+++ Infinity/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private GameObject player;
+	private float theY;
+
+	void Start (){
+		player = GameObject.Find ("Ship_player");
+	}
+
+
+	void Update () {
+		if (Input.GetKeyDown("up")) {
+			if (transform.position.y == 4) {
+				transform.position = transform.position;
+			} else {
+				theY = transform.position.y + 2;
+				transform.position = new Vector3(transform.position.x, theY, transform.position.z);
+			}
+		} 
+		else if(Input.GetKeyDown("down")){
+			if (transform.position.y == -4) {
+				transform.position = transform.position;
+			} else {
+				theY = transform.position.y - 2;
+				transform.position = new Vector3(transform.position.x, theY, transform.position.z);
+			}
+		}
+}
+}
