31b67d5c5576f585f76074307b696e66f2839580
jkoeni6 <jkoeni6@lsu.edu>
2017-03-20 23:06:32 -0500

    Added enemy and asteroid movement
    Added a simple enemy and asteroid movement sheet

---
+++ Infinity/Assets/Scripts/Enemy_Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Move : MonoBehaviour {
+    public float speed = 6.0f;
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+	}
+}
