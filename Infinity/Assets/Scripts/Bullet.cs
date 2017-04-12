c16a43f984ac32727e570d490904c6b80a022e03
Justin-Hester <jheste6@lsu.edu>
2017-04-11 19:40:23 -0500

    Fixed laserbeam and interactions
    Added interaction that destroys objects when a laserbeam hits them. Also
    modified the speed of the laserbeam and made it to where only one
    laserbeam can exists at a time.

--- Infinity/Assets/Scripts/Bullet.cs
+++ Infinity/Assets/Scripts/Bullet.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
-
-    public int speed = 1;
     Rigidbody2D bullet;
-
+    public static bool bulletExists = false;
 
     // Use this for initialization
-    void Start () {
-         bullet = GetComponent < Rigidbody2D > ();
-        
+    void Start ()
+    {
+        bullet = GetComponent < Rigidbody2D > ();   
     }
-	
 
 	// Update is called once per frame
-	void Update () {
-        bullet.transform.Translate(Vector3.right);
+	void Update ()
+    {
+        bullet.transform.Translate(Vector3.right * .25F);
     }
-
 
     void OnBecameInvisible()
     { 
         Destroy(gameObject);
+        bulletExists = false;
     }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject obj = other.transform.gameObject;
+        Destroy(obj);
+        Destroy(this.gameObject);
+        bulletExists = false;
+    }
 }
