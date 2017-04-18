19e0d18f7c095b722ddd7c0fea2657df1bebb091
Justin-Hester <jheste6@lsu.edu>
2017-04-18 17:54:41 -0500

    Score Working - small issue to be later fixed
    Changed default background from blue to black to better fit the space
    theme. Updated the scoring mechanic to reflect in the UI. Made "Enemey
    1" tag for enemy type 1 that take 1 hit to destroy and give 100 points.
    Likewise, I made an "Enemy 2" tag for enemy type 2 that takes 2 hits to
    destroy and gives 200 points. At this point though, All enemy type 2
    share the same health and I will have to later fix that. Lastly I made
    an "asteroid" tag for the asteroids. These objects cannot be destroyed
    by a laserbeam and must be avoided by the player.

---
+++ Infinity/Assets/Scripts/enemyHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealth : MonoBehaviour
+{
+    public static int health; // Health of the enemy
+
+	// Initialize health to 2 for "Enemy 2" tagged enemies
+	void Start()
+    {
+        health = 2;
+	}
+	
+
+	// Method to apply damage to the enemy
+	public static void applyDamage()
+    {
+        health -= 1;
+	}
+}
