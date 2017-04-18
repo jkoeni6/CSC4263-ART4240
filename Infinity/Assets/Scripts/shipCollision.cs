9d7934514aa73722de2f65a7c15419401670444f
Justin-Hester <jheste6@lsu.edu>
2017-04-18 15:08:24 -0500

    Quick Fixes
    Modified bullet speed and added a boolean that stops the enemy movement
    temporarily when the player loses a life. Also added comments to the
    scripts to better explain what is happening in the code.

--- Infinity/Assets/Scripts/shipCollision.cs
+++ Infinity/Assets/Scripts/shipCollision.cs
@@ -7,20 +7,28 @@ public class shipCollision : MonoBehaviour
     private GameObject ship1;
     public static int lives;
 
+    // Boolean to stop enemy movement when the player loses a life
+    public static bool shouldMove = true;
+
+    // Method that gives the ship 3 lives at the start of the game
     void Start()
     {
         ship1 = GameObject.Find("Ship_player");
         lives = 3;
     }
 
+    // Method to take away a life when the ship collides with another object
     void OnTriggerEnter2D(Collider2D other)
     {
-        lives--;
-        if (lives > 0)
+        shouldMove = false; // Stopping the enemy movement temporarily when the player loses a life
+        GameObject obj = other.transform.gameObject;
+        Destroy(obj); // Destroys the object the ship collided with
+        lives--; // Subtracts a life when the ship collides with another object
+        if (lives > 0) // If there are more lives left, keep playing
         {
             print(lives);
         }
-        else
+        else // Else, Game Over
         {
             Destroy(ship1);
             print(lives);
