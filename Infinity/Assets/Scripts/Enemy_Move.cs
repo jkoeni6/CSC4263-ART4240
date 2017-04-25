dbfd2a169a10117c59ed1653bc463db66357189a
Justin-Hester <jheste6@lsu.edu>
2017-04-24 23:04:19 -0500

    Created Endless Enemy Sheets
    Game now doesn't end until you run out of lives. Also increased the
    speed of newly incoming enemy sheets.

--- Infinity/Assets/Scripts/Enemy_Move.cs
+++ Infinity/Assets/Scripts/Enemy_Move.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Enemy_Move : MonoBehaviour
 {
-    public float speed = 6.0f;
+    public static float speed = 6.0f;
     public Transform target;
+    public GameObject Sheet;
+
+    // Booleans to spawn new sheet and stop FixedUpdate()
+    private bool shouldSpawn;
+    private bool stopUpdate;
+
+    void Start()
+    {
+        shouldSpawn = false;
+        stopUpdate = false;
+    }
 	
 	// Method that moves enemies towards player when shouldMove == true
 	void FixedUpdate()
     {
         if (shipCollision.shouldMove == true)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            if (!stopUpdate)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                if (transform.position == target.position)
+                {
+                    stopUpdate = true;
+                    shouldSpawn = true;
+                }
+            }
+
+            if (shouldSpawn)
+            {
+                spawnNewSheet();
+                shouldSpawn = false;
+            }
         }
 	}
+
+    // Method to destroy the bullet when it leaves the screen
+    public  void spawnNewSheet()
+    {
+        Sheet = Instantiate(Sheet, new Vector3(30, -1.93589F, 0), Quaternion.identity);
+        speed += 4.0f;
+    }
 }
